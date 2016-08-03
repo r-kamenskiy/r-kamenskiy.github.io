816242515e2e4e57833cbe2b1a8f7b4c7d0a7294
r.kamenskiy <r@kamenskiy.net>
2016-08-04 01:06:14 +0300

    social links works

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,21 @@
-
 $( document ).ready(function() {
+    $(".copyright").hide();
+    $(".social-detail").hide();
+    		
 	var btn = $("#collapseLeftPanel");
     btn.click(function(){
     	$(".left-panel-content").toggleClass("left-panel-expanded");
     	$(".left-panel").toggleClass("left-panel-expanded");
-
+    	$(".social-link").toggleClass("social-link-expanded");
+    	
     	if(btn.text().trim() === ">"){
+    		$(".copyright").show();
+    		$(".social-detail").show();
     		btn.text("<");
     	} else{
+    		$(".copyright").hide();
+    		$(".social-detail").hide();
+
     		btn.text(">");
     	}
     });
