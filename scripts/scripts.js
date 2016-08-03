57dda7dc9d4840b1ec50eb3f1fe2a108f9c6a1ba
r.kamenskiy <r@kamenskiy.net>
2016-08-04 01:11:24 +0300

    arrow fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,26 @@
 $( document ).ready(function() {
     $(".copyright").hide();
     $(".social-detail").hide();
-    		
+
 	var btn = $("#collapseLeftPanel");
     btn.click(function(){
     	$(".left-panel-content").toggleClass("left-panel-expanded");
     	$(".left-panel").toggleClass("left-panel-expanded");
     	$(".social-link").toggleClass("social-link-expanded");
-    	
-    	if(btn.text().trim() === ">"){
+
+    	if(btn.hasClass("fa-arrow-right")){
     		$(".copyright").show();
     		$(".social-detail").show();
-    		btn.text("<");
+    		$(".social-link").toggleClass("social-link-expanded");
+
+    		btn.removeClass("fa-arrow-right");
+    		btn.addClass("fa-arrow-left");
     	} else{
     		$(".copyright").hide();
     		$(".social-detail").hide();
 
-    		btn.text(">");
+    		btn.addClass("fa-arrow-right");
+    		btn.removeClass("fa-arrow-left");
     	}
     });
 });
